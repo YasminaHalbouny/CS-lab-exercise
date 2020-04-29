da11147856ea0e324b42ee5cb5b500b3ef7270fc
Mazen_Hassan <mhassan20328@aucegypt.edu>
2020-04-29 12:19:50 +0200

    testingv2

--- sources/mhassan20328.cpp
+++ sources/mhassan20328.cpp
@@ -1,7 +1,7 @@
-#include iostream
+#include <iostream>
 #include "mhassan20328.h"
 using namespace std;
-void mhassan20328:mhassan20328()
+void mhassan20328::mhassan20328()
 {
 	cout<<"Mazen Hassan"<<endl;
 	cout<"mhassan20328@aucegypt.edu";
